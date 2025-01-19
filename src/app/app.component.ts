1528dbb54bdf90497b667e5dd9dc175928ceb55d
deepakmahajan57 <deepakmahajan57@gmail.com>
2025-01-19 17:36:03 +0530

    spinner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DSpinnerService } from 'd-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'd-spinner';
+  
+  spinnerTypes = [
+    'simple-spinner',
+    'inner-arc',
+    'outer-arc',
+    'twin-inner-arc',
+    'twin-outer-arc',
+    'inner-orbiting-dot',
+    'outer-orbiting-dot',
+    'inner-spiral',
+    'dual-segment',
+    'dotted-circular',
+    'rotating-bar',
+    'ring',
+    'square-wave',
+    'glowing-ring',
+    'half-ring',
+    'intersecting-ring',
+    'balancing-shapes',
+    'rotating-slice',
+    'inner-fill-arc',
+    'rolling-circle',
+    'orbiting-dots',
+    'dual-pulsating-dots',
+    'expanding-dots',
+    'rotating-double-square',
+    'scattered-diamond',
+    'pulsating-grid',
+    'expanding-square',
+    'tilted-bar',
+    'bouncing-diamond',
+    'intersecting-triangle',
+    'rotating-triangle',
+    'bouncing-square',
+    'spin-with-bounce',
+    'circle-inside-square',
+    'vertical-bars-pulsation',
+    'flipping-parallel-lines',
+    'rising-bars-with-dot',
+    'dashed-line-expansion',
+    'balancing-capsule',
+    'loading-bounce',
+    'expanding-block',
+    'arc-of-dots',
+    'rotating-pie-shape',
+    'circular-progress',
+    'bouncing-dots'
+  ];
+
+  constructor(private spinner: DSpinnerService) {
+    this.spinner.show('allSpinner');
+
+  }
+
+  // Method to show the spinner
+  showSpinner(card : any) {
+    this.spinner.show(card);
+  }
+
+  // Method to hide the spinner
+  hideSpinner(card : any) {
+    this.spinner.hide(card);
+  }
+
+
 }
